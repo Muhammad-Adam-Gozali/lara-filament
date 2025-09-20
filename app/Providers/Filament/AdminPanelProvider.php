a26f77f3955d2b0c675067d22ec9d2279f413fa0
Muhammad-Adam-Gozali <adamgozalimuhammad@gmail.com>
2025-09-20 15:17:52 +0700

    Remove filament-shield package

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -62,9 +62,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])
-            ->plugins([
-                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make(),
             ]);
     }
 }
